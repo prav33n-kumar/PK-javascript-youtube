68eeda94303e1395f04501af6f51914b308f7ea2
prav33n-kumar <167536609+prav33n-kumar@users.noreply.github.com>
2024-04-19 13:42:31 +0000

    data types discussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node.js, not browser
+
+console.log(3 + 
+    3)              //code readability issue
+console.log("Praveen")
+
+let name = "Praveen"
+let age = 22
+let isLoggedIn = false
+let state
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness - mostly used in react
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
